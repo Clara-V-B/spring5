3fca1f322a8449ae43b0920731668151056f7854
Clara Bernasconi <bernasconiclara00@gmail.com>
2022-09-06 14:10:20 -0300

    CRUD con Spring API Rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
-import { ClientesComponent } from './clientes/clientes.component';
+import { ClientesComponent } from './clientes/clientes.component';//componente que tiene angular para la comunicacion con el servidor remoto
+import { FormComponent } from './clientes/form/form.component';
+import { FormsModule } from '@angular/forms';
 
 //SERVICIOS
 import { ClienteService } from './clientes/cliente.service';
@@ -16,8 +18,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent},
+  {path: 'clientes/form/:id', component: FormComponent}
 ];
+
+//permite que en la clase service conectarme con el servidor
+import { HttpClientModule } from '@angular/common/http';
+//a traves de peticiones http
+
 
 @NgModule({
   declarations: [
@@ -25,10 +34,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,18 +1,42 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
+//import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente'
-import { of, Observable } from 'rxjs';
-
+//import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ClienteService {
+  urlEndPoint: string = 'http://localhost:8091/api/clientes';
+  httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    // return this.http.get<Cliente[]>(this.urlEndPoint);
+    return this.http.get(this.urlEndPoint).pipe(
+      map( (response) => response as Cliente[])
+    );
   };
+
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+  }
+
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+  }
+  
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+  }
+  
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+  }
 }
